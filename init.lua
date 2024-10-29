5d84f5083b55d499f126e27532979146a7bf1765
Ryan B. <rybott5000.5@gmail.com>
2024-10-29 00:33:20 -0400

    sds

--- init.lua
+++ init.lua
@@ -899,7 +899,7 @@ require('lazy').setup({
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -942,7 +942,6 @@ require('lazy').setup({
   -- { import = 'custom.plugins' },
 
   -- My Personal Plugins
-
   {
     'glepnir/dashboard-nvim',
     event = 'VimEnter',
@@ -998,14 +997,41 @@ require('lazy').setup({
     'MeanderingProgrammer/render-markdown.nvim',
     main = 'render-markdown',
     opts = {},
-    name = 'render-markdown',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    render_modes = true,
+    heading = {
+      enabled = true,
+      sign = true,
+      position = 'overlay',
+      icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' },
+      signs = { '󰫎 ' },
+      width = 'full',
+      left_margin = 0,
+      left_pad = 0,
+      right_pad = 0,
+      min_width = 0,
+      border = false,
+      border_virtual = false,
+      border_prefix = false,
+      above = '▄',
+      below = '▀',
+      backgrounds = {
+        'RenderMarkdownH1Bg',
+        'RenderMarkdownH2Bg',
+        'RenderMarkdownH3Bg',
+        'RenderMarkdownH4Bg',
+        'RenderMarkdownH5Bg',
+        'RenderMarkdownH6Bg',
+      },
+      foregrounds = {
+        'RenderMarkdownH1',
+        'RenderMarkdownH2',
+        'RenderMarkdownH3',
+        'RenderMarkdownH4',
+        'RenderMarkdownH5',
+        'RenderMarkdownH6',
+      },
+    },
   },
-
-  -- Put your Keybindings Here
-  --
 
   vim.api.nvim_set_keymap('n', '<leader>zf', 'z=<CR>', { noremap = true, silent = true }),
 
